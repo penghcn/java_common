7977c4a6f03c72a6612f3a80d93dc6fe2cf57ef0
penghcn <qingseyunlan@gmail.com>
2016-06-17 16:42:02 +0800

    Update BeanPostHelper.java

--- helper/BeanPostHelper.java
+++ helper/BeanPostHelper.java
@@ -1,10 +1,14 @@
+package cn.pengh.helper;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
+import cn.pengh.annotation.ExceptField;
+import cn.pengh.library.Log;
+import cn.pengh.util.StringUtil;
 /**
  * 
  * @author penghcn
@@ -17,19 +21,32 @@ public class BeanPostHelper {
 	public static String toUpdateSet(Object obj,String... except) {
 		Class<?> clazz = obj.getClass();
 		String s = toUpdateSetInternal(obj,clazz,except) + getSuperRecursive(obj, clazz, except);
-		s = s.endsWith(",") ? s.substring(0,s.length() - 2) : s;
+		//s = s.endsWith(",") ? s.substring(0,s.length() - 1) : s;
+		s = subEndRecursive(s);
 		
 		
 		if (s == null || s.equals("")){
-			LogWriter.error("生成update set语句为空");
+			Log.error("生成update set语句为空");
 		}
 		return " set "+s;
+	}
+	
+	private static String subEndRecursive(String s){
+		s = s.trim();
+		if(s.endsWith(",")){
+			s = s.substring(0,s.length() -1);
+			s = subEndRecursive(s);
+		} else {
+			return s;
+		}
+		return s;
 	}
 	
 	private static String getSuperRecursive(Object obj,Class<?> clazz,String... except){
 		String s = "";
 		if (s.equals("") && clazz.getSuperclass() != null){
 			String superStr = toUpdateSetInternal(obj,clazz.getSuperclass(),except);
+			superStr = subEndRecursive(superStr);
 			s += superStr == null || superStr.equals("") ? "" : superStr+",";
 			s += getSuperRecursive(obj, clazz.getSuperclass(), except);
 		} else {
@@ -69,7 +86,7 @@ public class BeanPostHelper {
 			f.setAccessible(false);
 			
 			// set ins_cd = :insCd
-			sb.append(u2l(k)+" = :"+k+",");
+			sb.append(StringUtil.toUnderlineCase(k)+" = :"+k+",");
 			
 		}
 		
@@ -77,6 +94,17 @@ public class BeanPostHelper {
 	}
 	
 	public static String toInsertSql(Object obj,String... except) {
+		return toInsertSql(obj, true, true, except);
+	}
+	/**
+	 * 
+	 * @param obj
+	 * @param isPrepare 是否是预编译形式 :userName
+	 * @param isUnderline 是否由驼峰转为下划线模式 user_name
+	 * @param except
+	 * @return
+	 */
+	public static String toInsertSql(Object obj,boolean isPrepare,boolean isUnderline,String... except) {
 		Class<?> clazz = obj.getClass();
 		List<String> ls = toInsertSqlInternal(obj,clazz,except);
 		if (clazz.getSuperclass() != null) {
@@ -89,13 +117,22 @@ public class BeanPostHelper {
 		StringBuilder sbv = new StringBuilder();
 		for (String key : ls) {
 			if (key.endsWith(_SEQUENCE_)){
-				key = u2l(key.replaceAll(_SEQUENCE_, ""));
+				key = StringUtil.toUnderlineCase(key.replaceAll(_SEQUENCE_, ""));
 				sbk.append(","+key);
 				sbv.append(",next value for "+ key);
 				continue;
 			}
-			sbk.append(","+u2l(key));
-			sbv.append(",:"+key);
+
+			sbk.append(","+(isUnderline ? StringUtil.toUnderlineCase(key) : key));
+			if (isPrepare) {				
+				sbv.append(",:"+key);
+			} else {
+				if ("String".equals(ClazzHelper.getFieldTypeByMethod(clazz, key)))
+					sbv.append(",'"+ClazzHelper.getObjVal(obj, key)+"'");
+				else 
+					sbv.append(","+ClazzHelper.getObjVal(obj, key));
+			}
+			
 		}
 		if(sbk.length() == 0)
 			throw new IllegalArgumentException("解析insert sql语句出错。");
@@ -165,6 +202,9 @@ public class BeanPostHelper {
 			// 如果是编译器引入的则跳过
 			if (f.isSynthetic() || f.isEnumConstant())
 				continue;
+			// static则跳过
+			if (Modifier.isStatic(f.getModifiers()))
+				continue;
 			if (Arrays.asList(except).contains(k))
 				continue;
 			
@@ -181,7 +221,7 @@ public class BeanPostHelper {
 			f.setAccessible(false);
 			
 			// and inf.ins_cd = :insCd
-			sb.append(" and "+pre+u2l(k) + "= :"+k);
+			sb.append(" and "+pre+StringUtil.toUnderlineCase(k) + "= :"+k);
 			
 		}
 		return sb.toString();
@@ -197,19 +237,6 @@ public class BeanPostHelper {
 		return "SELECT * FROM(" + sql +") AS TBS_LIMIT WHERE TBS_LIMIT.RN BETWEEN :start AND :end";
 	}
 	
-	private static String u2l(String str) {
-        StringBuffer sb = new StringBuffer();        
-        for(int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if(Character.isUpperCase(ch)) {
-            	sb.append("_"+ Character.toLowerCase(ch));
-            }
-            else {
-            	sb.append(ch);
-            }
-        }
-        return sb.toString();
-	}
 	
 	
 	
